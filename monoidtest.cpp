7bb53fa59712b462da439044b5dc1c191e7e0341
JoeDumoulin <joe.dumoulin@gmail.com>
2014-10-18 11:22:36 -0700

    added code for function composition monoid and simple poc in main

--- monoidtest.cpp
+++ monoidtest.cpp
@@ -5,15 +5,25 @@
 
 #include <iostream>
 #include <functional>
+
 #include "monoid.hpp"
+#include "function_composition_monoid.hpp"
 
 int main(int agrc, const char** argv)
 {
+
+  artin::monoid<artin::unary_func<int> > cm = artin::function_composition_monoid<int>::mon();
+  const artin::unary_func<int> fx([](int a){return a+a;});
+  const artin::unary_func<int> gx([](int b){return b*b;});
+  auto r = cm.Op(fx, gx)(3);
+
+
   static artin::monoid<int> integer_additive_monoid(std::plus<int>(), 0);
   static artin::monoid<int> integer_multiplicative_monoid(std::multiplies<int>(), 0);
 
   std::cout << "answer: " << integer_additive_monoid.Op(1, 1) << std::endl;
   std::cout << "answer: " << integer_multiplicative_monoid.Op(1, 1) << std::endl;
+  std::cout << "answer: " << r << std::endl;
   return 0;
 
 }
